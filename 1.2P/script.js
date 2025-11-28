721882d3f7f66556728790b7fb1f92a2aabf1125
NavyaAila <s225272001@deakin.edu.au>
2025-11-28 14:18:28 +1100

    Delete 1.2P directory

--- 1.2P/script.js
+++
@@ -1,8 +0,0 @@
-function checkPalindrome() {
-    const text = document.getElementById("textInput").value;
-    const reversed = text.split('').reverse().join('');
-    const isPalindrome = text === reversed;
-    
-    document.getElementById("result").textContent = 
-        isPalindrome ? "Palindrome" : "Not a Palindrome";
-}
